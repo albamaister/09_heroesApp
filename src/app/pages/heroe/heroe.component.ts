7c5555b4c4559479a8e6d5ed2f844b0d176915c5
Andres Alba <bryanalba92@gmail.com>
2019-08-10 21:08:33 -0500

    Información del estado de las peticiones

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-heroe',
@@ -23,15 +26,35 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando Información',
+      type: 'info',
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+    let textoInformacion;
+
     if (this.heroe.id) {
-      this.heroesService.actualizarHeroe(this.heroe).subscribe( resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
+      textoInformacion = 'Se actualizó correctamente';
     } else {
-      this.heroesService.crearHeroe(this.heroe).subscribe( resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.crearHeroe(this.heroe);
+      textoInformacion = 'Se creó correctamente';
     }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: textoInformacion,
+        type: 'success'
+      });
+    });
+
+
   }
 
 }
